5861ab6a14f5fe74b1d4a57444c7d4800ee8e832
Hackids Education <65713501+HackidsEdu@users.noreply.github.com>
2021-03-28 11:52:38 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+HackbitOLEDDisplay.init(128, 64)
+HackbitOLEDDisplay.writeStringNewLine("hack:bit")
+HackbitOLEDDisplay.writeStringNewLine("Estacao Metereologica")
+HackbitOLEDDisplay.writeStringNewLine("DHT11|BMP280|OLED")
+basic.pause(3000)
+basic.forever(function () {
+    hackbit.queryData(
+    hackbit.DHTtype.DHT11,
+    DigitalPin.P13,
+    true,
+    false,
+    true
+    )
+})
+basic.forever(function () {
+    if (hackbit.readDataSuccessful()) {
+        HackbitOLEDDisplay.clear()
+        HackbitOLEDDisplay.writeStringNewLine("hack:bit")
+        HackbitOLEDDisplay.writeStringNewLine("Estacao Metereologica")
+        HackbitOLEDDisplay.writeStringNewLine("DHT11|BMP280|OLED")
+        HackbitOLEDDisplay.writeString("Umid.:")
+        HackbitOLEDDisplay.writeNumNewLine(hackbit.readData(hackbit.dataType.humidity))
+        HackbitOLEDDisplay.writeString("Temp.: ")
+        HackbitOLEDDisplay.writeNumNewLine(hackbit.getTemperatureDecimal())
+        HackbitOLEDDisplay.writeString("Pressao:")
+        HackbitOLEDDisplay.writeNumNewLine(hackbit.getPressureDecimal())
+        basic.pause(5000)
+    }
+})
